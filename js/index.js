225b4c804fe6b4dbc16ce21d07e0f8c281b70236
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2021-05-13 21:32:04 +0700

    overall the app modified

--- js/index.js
+++ js/index.js
@@ -2,26 +2,21 @@
 //Side menu toggle
 const openMenu = document.querySelector('#menu-toggle');
 const closeMenu = document.querySelector('#close-menu');
-const navbackground = document.querySelector('.nav-bg');
-const homeSection = document.querySelector('.home-section');
+const mainContainer = document.querySelector('.main');
 const menu = document.querySelector('.menu');
 openMenu.addEventListener('click', () => {
   menu.classList.add('active-menu');
-  homeSection.style.display = 'none'; 
-  navbackground.style.display = 'block'; 
   // add listener to disable scroll
   window.addEventListener('scroll', noScroll);
 });
 
 //Closing the Menu
 closeMenu.addEventListener('click', closeMenuToggle);
-navbackground.addEventListener('click', closeMenuToggle);
+mainContainer.addEventListener('click', closeMenuToggle);
 
 //Close Menu function
 function closeMenuToggle() {
   menu.classList.remove('active-menu');
-  navbackground.style.display = 'none';
-  homeSection.style.display = 'block'; 
   // Remove listener to re-enable scroll
   window.removeEventListener('scroll', noScroll);
 };
